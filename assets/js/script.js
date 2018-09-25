17d2b641177bc55bd06384830ef7041247c3bcd2
Daniel A <dalamo90@gmail.com>
2018-09-25 18:33:02 -0500

    saving menu adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
+// function myFunction(x) {
+//     x.classList.toggle("change");
+// }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction(x) {
+    document.getElementById("myDropdown").classList.toggle("show");
     x.classList.toggle("change");
 }
 
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
