42b9827a14b3a3ce946f98156b1cc6abad9affe9
Daniel A <dalamo90@gmail.com>
2018-09-25 15:44:01 -0500

    menu opens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
