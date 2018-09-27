5fd82e292871421c80b84588ce8e4609797e4bdd
Daniel A <dalamo90@gmail.com>
2018-09-27 16:44:22 -0500

    side menu transitions, menu icon changes color, menu takes full height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,70 @@
-// function myFunction(x) {
-//     x.classList.toggle("change");
-// }
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction(x) {
-    document.getElementById("myDropdown").classList.toggle("show");
     x.classList.toggle("change");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// $(".btn-toggle-nav").click(function() {
+//     $(".btn-toggle-nav").toggle("active");
+// });
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function myFunction(x) {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//     x.classList.toggle("change");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 
 //THIS IS PRACTICE 
 
 let toggleNavStatus = false;
 
 let toggleNav = function() {
+    let getBarOne = document.querySelector(".bar1");
+    let getBarTwo = document.querySelector(".bar2");
+    let getBarThree = document.querySelector(".bar3");
     let getSidebar = document.querySelector(".nav-sidebar");
     let getSidebarUl = document.querySelector(".nav-sidebar ul");
     let getSidebarTitle = document.querySelector(".nav-sidebar span");
     let getSidebarLinks = document.querySelectorAll(".nav-sidebar a"); //querySelectorAll puts list in array
 
     if (toggleNavStatus === false) {
+        getBarOne.style.background = "white";
+        getBarTwo.style.background = "white";
+        getBarThree.style.background = "white";
         getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "272px";
         getSidebarTitle.style.opacity = "0.5";
-
+        
         let arrayLength = getSidebarLinks.length;
-
+        
         for (let i=0; i < arrayLength; i++) {
             getSidebarLinks[i].style.opacity = "1";
         }
+        
+        toggleNavStatus = true;
 
-        toggleNavStatus === true;
     }
-
     else if (toggleNavStatus === true) {
-
-        getSidebar.style.width = "50px";
+        getBarOne.style.background = "#333";
+        getBarTwo.style.background = "#333";
+        getBarThree.style.background = "#333";
+        getSidebar.style.width = "0";
+        getSidebar.style.padding = "0;"
         getSidebarTitle.style.opacity = "0";
 
         let arrayLength = getSidebarLinks.length;
@@ -58,10 +72,9 @@ let toggleNav = function() {
         for (let i=0; i < arrayLength; i++) {
             getSidebarLinks[i].style.opacity = "0";
         }
-
+        
         getSidebarUl.style.visibility = "hidden";
 
         toggleNavStatus = false;
-    }
-
-}
+    };
+};
