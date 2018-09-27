497e319de186375dc7461ca5366df972e5704292
Daniel A <dalamo90@gmail.com>
2018-09-27 17:01:18 -0500

    burger menu changes shape on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,6 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
-// $(".btn-toggle-nav").click(function() {
-//     $(".btn-toggle-nav").toggle("active");
-// });
-
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-// function myFunction(x) {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//     x.classList.toggle("change");
-// }
-
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
-
 //THIS IS PRACTICE 
 
 let toggleNavStatus = false;
